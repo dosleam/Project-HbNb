02b853aaac980f2326d7c42f97820561b46fff1d
gdbmrm <>
2024-10-21 11:11:17 +0000

    task 3 mariama api/v1/amenities  implementation de get et post

---
+++ part2/APP/api/v1/amenities.py
@@ -0,0 +1,55 @@
+from flask_restx import Namespace, Resource, fields
+from app.services.facade import HBnBFacade
+
+api = Namespace('amenities', description='Amenity operations')
+
+# Define the amenity model for input validation and documentation
+amenity_model = api.model('Amenity', {
+    'name': fields.String(required=True,
+    description='Name of the amenity')
+})
+
+amenities = []
+facade = HBnBFacade()
+
+@api.route('/')
+class AmenityList(Resource):
+    @api.expect(amenity_model)
+    @api.response(201, 'Amenity successfully created')
+    @api.response(400, 'Invalid input data')
+    def post(self):
+        """Register a new amenity"""
+        amenity = request.get_json()
+
+        if not amenity or 'name' not in amenity:
+            return jsonify({"error":"Invalid input data"}), 400
+        
+        for amenity_registered in amenities:
+            if amenity_registered == amenity:
+                return jsonify({"error": "Amenity already registered"}), 400
+
+        amenities.append(amenity)
+
+    @api.response(200, 'List of amenities retrieved successfully')
+    def get(self):
+        """Retrieve a list of all amenities"""
+        return amenities, 200
+
+
+@api.route('/<amenity_id>')
+class AmenityResource(Resource):
+    @api.response(200, 'Amenity details retrieved successfully')
+    @api.response(404, 'Amenity not found')
+    def get(self, amenity_id):
+        """Get amenity details by ID"""
+        # Placeholder for the logic to retrieve an amenity by ID
+        pass
+
+    @api.expect(amenity_model)
+    @api.response(200, 'Amenity updated successfully')
+    @api.response(404, 'Amenity not found')
+    @api.response(400, 'Invalid input data')
+    def put(self, amenity_id):
+        """Update an amenity's information"""
+        # Placeholder for the logic to update an amenity by ID
+        pass
