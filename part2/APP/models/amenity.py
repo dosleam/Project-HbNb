c0081d4a6d8e70d59e33e1f21c7a48f354cefb86
gdbmrm <>
2024-10-22 09:22:56 +0000

    implementation des fonctions dans amenity.py

--- part2/APP/models/amenity.py
+++ part2/APP/models/amenity.py
@@ -6,3 +6,17 @@ class Amenity(BaseModel):
         if len(name) > 50:
             raise ValueError("Name must not exceed 50 characters")
         self.name = name
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.user_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        
