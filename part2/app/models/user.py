61287428c491dd04dd13e167fa8bb3cdea19688e
dosleam <9400@holbertonstudents.com>
2024-11-08 11:15:43 +0100

    update user.py

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,12 +1,14 @@
 from .base import BaseModel
+from app import bcrypt
 import re
 
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email, is_admin=False):
+    def __init__(self, first_name, last_name, email, password, is_admin=False):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        self.password = password  # Utilise le setter pour hacher le mot de passe
         self.is_admin = is_admin
         self.places = []
 
@@ -18,7 +20,6 @@ class User(BaseModel):
     def first_name(self, value):
         if value is None:
             raise ValueError("You must enter a first name")
-
         if len(value) > 50:
             raise ValueError("First name must not exceed 50 characters")
         self._first_name = value
@@ -31,7 +32,6 @@ class User(BaseModel):
     def last_name(self, value):
         if value is None:
             raise ValueError("You must enter a last name")
-
         if len(value) > 50:
             raise ValueError("Last name must not exceed 50 characters")
         self._last_name = value
@@ -44,10 +44,24 @@ class User(BaseModel):
     def email(self, value):
         if value is None:
             raise ValueError("You must enter an email")
-
         if not re.match(r"[^@]+@[^@]+\.[^@]+", value):
-            raise ValueError("Invalid input data")
+            raise ValueError("Invalid email format")
         self._email = value
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        if value is None or len(value) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        # Hacher le mot de passe avant de le stocker
+        self._password = bcrypt.generate_password_hash(value).decode('utf-8')
+
+    def verify_password(self, password):
+        """Vérifie si le mot de passe fourni correspond au mot de passe haché."""
+        return bcrypt.check_password_hash(self._password, password)
 
     def add_place(self, place):
         """Add a place to the user"""
