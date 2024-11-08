d9554ba10d3755df2bbb927731d322d88083eddd
dosleam <9400@holbertonstudents.com>
2024-11-08 11:10:33 +0100

    update init and requirement

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -5,17 +5,25 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
+    # Création de l'application Flask
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+
+    # Chargement de la configuration à partir de l'objet de configuration
+    app.config.from_object(config_class)
+
+    # Initialisation de l'API
+    api = Api(
+        app,
+        version='1.0',
+        title='HBnB API',
+        description='HBnB Application API'
+    )
+
+    # Ajout des namespaces pour les différents modules
     api.add_namespace(reviews_ns, path="/api/v1/reviews")
     api.add_namespace(users_ns, path="/api/v1/users")
     api.add_namespace(places_ns, path="/api/v1/places")
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
-
-
-
-    # Placeholder for API namespaces (endpoints will be added later)
-    # Additional namespaces for places, reviews, and amenities will be added later
 
     return app
